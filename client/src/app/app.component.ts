bb8c95ecc26372a8eae78096771abf0130bcc970
Tyler Harris <tharris@assyst.net>
2025-04-25 14:02:28 -0400

    Added svg icons to assets; Added initial hz themes; Created icon service

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,7 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TokenService } from './core/services/token.service';
-import { MatIconRegistry } from '@angular/material/icon';
-import { DomSanitizer } from '@angular/platform-browser';
-import { GITHUB_ICON, LINKEDIN_ICON } from './core/constants/icon-definitions.constant';
+import { IconService } from './core/services/icon.service';
 
 @Component({
   selector: 'app-root',
@@ -13,19 +11,13 @@ import { GITHUB_ICON, LINKEDIN_ICON } from './core/constants/icon-definitions.co
 })
 export class AppComponent implements OnInit {
   private tokenService = inject(TokenService);
+  private iconService = inject(IconService);
 
-  private matIconRegistry = inject(MatIconRegistry);
-  private domSanitizer = inject(DomSanitizer);
+  constructor() {
+    this.iconService.registerIcons();
+  }
 
   async ngOnInit() {
     await this.tokenService.onInitUser();
-    this.registerIcons();
   }
-
-  private registerIcons() {
-    this.matIconRegistry.addSvgIconLiteral('github', this.domSanitizer.bypassSecurityTrustHtml(GITHUB_ICON));
-    this.matIconRegistry.addSvgIconLiteral('linkedin', this.domSanitizer.bypassSecurityTrustHtml(LINKEDIN_ICON));
-  }
-
-  title = 'horizon';
 }
---
+++ client/src/app/core/services/icon.service.ts
@@ -0,0 +1,16 @@
+import { inject, Injectable } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class IconService {
+  private matIconRegistry = inject(MatIconRegistry);
+  private domSanitizer = inject(DomSanitizer);
+
+  registerIcons() {
+    this.matIconRegistry.addSvgIcon('github', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/github-brands.svg'));
+    this.matIconRegistry.addSvgIcon('linkedin', this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/linkedin-brands.svg'));
+  }
+}
