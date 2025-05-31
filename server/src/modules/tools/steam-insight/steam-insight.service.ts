06b10c9b270961e8ab75762e8383d8b88b1d83cd
tchar <t@c>
2025-05-30 20:46:23 -0400

    Fixed steam insight - disabled adult filter temporarily

--- server/src/modules/tools/steam-insight/steam-insight.service.ts
+++ server/src/modules/tools/steam-insight/steam-insight.service.ts
@@ -13,15 +13,14 @@ export class SteamInsightService {
   ) {}
 
   async getSteamGames(options?: SteamAppSearchOptions): Promise<SteamAppSearchInfo> {
-    /*
     const { pageIndex = 0, search, allowAdultContent = false } = options;
     const skip = pageIndex * STEAM_INSIGHT_PAGE_SIZE;
 
     // Setup Keyword Query
     const qb = this.steamAppRepository.createQueryBuilder('app');
     qb.select(['app.appid', 'app.name', 'app.headerImage', 'app.shortDescription', 'app.categories']);
-    qb.where('app.type = :type AND app.is_adult = :isAdult', { type: 'game', isAdult: false });
-    //qb.where('app.type = :type', { type: 'game' });
+    //qb.where('app.type = :type AND app.is_adult = :isAdult', { type: 'game', isAdult: false });
+    qb.where('app.type = :type', { type: 'game' });
     qb.skip(skip).take(STEAM_INSIGHT_PAGE_SIZE);
     qb.orderBy('app.appid', 'DESC');
 
@@ -43,41 +42,6 @@ export class SteamInsightService {
     return {
       pageLength: total,
       steamGames,
-    };*/
-
-    const { pageIndex = 0, search, allowAdultContent = false } = options;
-    const skip = pageIndex * STEAM_INSIGHT_PAGE_SIZE;
-
-    const qb = this.steamAppRepository.createQueryBuilder('app');
-    qb.select(['app.appid', 'app.name', 'app.headerImage', 'app.shortDescription', 'app.categories']);
-    qb.where('app.type = :type AND app.is_adult = :isAdult', { type: 'game', isAdult: false });
-    qb.skip(skip).take(STEAM_INSIGHT_PAGE_SIZE);
-    qb.orderBy('app.appid', 'DESC');
-
-    if (search) {
-      const keywords = search
-        .trim()
-        .split(/\s+/)
-        .map((word) => word.toLowerCase());
-
-      keywords.forEach((word, index) => {
-        qb.andWhere(`app.name ILIKE :kw${index}`, {
-          [`kw${index}`]: `%${word}%`,
-        });
-      });
-    }
-
-    // Extract SQL and parameters
-    const [sql, params] = qb.getQueryAndParameters();
-
-    // Run EXPLAIN ANALYZE using native query
-    const result = await this.steamAppRepository.query(`EXPLAIN (ANALYZE, BUFFERS, VERBOSE, FORMAT TEXT) ${sql}`, params);
-
-    console.log(result.map((r) => r['QUERY PLAN']).join('\n'));
-
-    return {
-      pageLength: 0,
-      steamGames: [], 
     };
   }
 
