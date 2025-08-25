7893a8f55a2929caf446114282e46e11af3d5934
Tyler Harris <tharris@assyst.net>
2025-08-25 17:08:24 -0400

    Code cleanup and refactor - Final; Updated todo.txt file with future plans; Added inital new themes

--- resources/notes/run_project_breakdown.ps1
+++
@@ -1,5 +0,0 @@
-$files = git ls-files --cached --others --exclude-standard
-$files | Sort-Object | ForEach-Object {
-    $depth = ($_ -split '[\\/]').Count - 1
-    ('  ' * $depth) + ($_ -split '[\\/]' | Select-Object -Last 1)
-} | Out-File -Encoding UTF8 project-structure.txt
